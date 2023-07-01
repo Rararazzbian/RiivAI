bf32d26707b00834a309318ad12f445be315834a
Razzbian <69510784+Rararazzbian@users.noreply.github.com>
2023-07-01 21:51:07 +0100

    Add files via upload

--- plugins/google_search/google_search.py
+++ plugins/google_search/google_search.py
@@ -3,11 +3,23 @@ from googlesearch import search
 import googleapiclient.discovery
 
 # Define the run function that takes a query and returns a string with the results
-def run(prompt):
+def run(variables_list, prompt):
+    print(variables_list)
+    # Extract the desired variables from the variables_list
+    data = {}
+    for line in variables_list.strip().split("\n"):
+        key, value = line.split(" = ")
+        key = key.strip()
+        value = value.strip("{}")
+        data[key] = value
+    
+    # Access the variables you need
+    GOOGLESEARCH_API_KEY = data.get("GOOGLESEARCH_API_KEY")
+    GOOGLESEARCH_CSE_ID = data.get("GOOGLESEARCH_CSE_ID")
     # Create a service object using the API key and the CSE ID
     service = googleapiclient.discovery.build("customsearch", "v1", developerKey=GOOGLESEARCH_API_KEY)
     # Execute the search request and get the response
-    response = service.cse().list(q=prompt, cx=GOOGLE_CSE_ID, num=5).execute()
+    response = service.cse().list(q=prompt, cx=GOOGLESEARCH_CSE_ID, num=5).execute()
     # Extract the list of items from the response
     items = response.get("items", [])
     # Initialize an empty string to store the output
